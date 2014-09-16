e54ff9e5a80d0689a15c2a1b02a3411c29f6c88b
Changhyun lee <leechwin1@gmail.com>
2014-09-17 04:27:08 +0900

    Added history

--- js/listener.js
+++ js/listener.js
@@ -355,6 +355,7 @@ function Alert(soundID, timestamp) {
 function History(soundID, timestamp) {
 	this.soundID = soundID;
     this.timestamp = timestamp;
+    this.currentStamp = null;
 }
 
 function addNewHistory(soundID, timestamp) {
